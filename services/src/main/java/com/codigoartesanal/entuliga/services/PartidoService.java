4e03a73318ebed32b950d7c3b6a46f92ac5b27a1
betuzo <kkimvazquezangeles@hotmail.com>
2015-06-10 23:12:12 -0500

    Service

---
+++ services/src/main/java/com/codigoartesanal/entuliga/services/PartidoService.java
@@ -0,0 +1,20 @@
+package com.codigoartesanal.entuliga.services;
+
+/**
+ * Created by kkimvazquezangeles on 10/06/15.
+ */
+public interface PartidoService {
+    public static final String PROPERTY_ID                  = "id";
+    public static final String PROPERTY_JORNADA_ID          = "jornadaId";
+    public static final String PROPERTY_JORNADA_NOMBRE      = "jornadaNombre";
+    public static final String PROPERTY_LOCAL_ID            = "localId";
+    public static final String PROPERTY_LOCAL_NOMBRE        = "localNombre";
+    public static final String PROPERTY_LOCAL_PUNTOS        = "localPuntos";
+    public static final String PROPERTY_VISITANTE_ID        = "visitanteId";
+    public static final String PROPERTY_VISITANTE_NOMBRE    = "visitanteNombre";
+    public static final String PROPERTY_VISITANTE_PUNTOS    = "visitantePuntos";
+    public static final String PROPERTY_CANCHA_ID           = "canchaId";
+    public static final String PROPERTY_CANCHA_NOMBRE       = "canchaNombre";
+    public static final String PROPERTY_HORARIO             = "horario";
+    public static final String PROPERTY_STATUS_PARTIDO      = "statusPartido";
+}
---
+++ services/src/main/java/com/codigoartesanal/entuliga/services/impl/PartidoServiceImpl.java
@@ -0,0 +1,11 @@
+package com.codigoartesanal.entuliga.services.impl;
+
+import com.codigoartesanal.entuliga.services.PartidoService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by kkimvazquezangeles on 10/06/15.
+ */
+@Service
+public class PartidoServiceImpl implements PartidoService {
+}
