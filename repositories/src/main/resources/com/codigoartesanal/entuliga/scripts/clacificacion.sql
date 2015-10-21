549bbca60a064782045ce5fcd91940af7612e7f5
rolguin <rolguin@bursatec.com.mx>
2015-10-21 18:27:10 -0500

    scripts para h2

--- repositories/src/main/resources/com/codigoartesanal/entuliga/scripts/clacificacion.sql
+++ repositories/src/main/resources/com/codigoartesanal/entuliga/scripts/clacificacion.sql
@@ -1,61 +1,64 @@
 Create view ClasificacionView as
 
-SELECT
-	torneoequipo.equipo_id,
-	equipo.nombre,
+SELECT 
+	torneoequipo.equipo_id, 
+	equipo.nombre, 
 	totallocal.torneo_id,
 
-	totallocal.puntos_local,
-	totallocal.puntos_local_contra,
-	totallocal.ganados_local,
-	totallocal.perdidos_local,
-	totallocal.jugados_local,
-
-	totalvisita.puntos_visita,
-	totalvisita.puntos_visita_contra,
-	totalvisita.ganados_visita,
-	totalvisita.perdidos_visita,
-	totalvisita.jugados_visita,
-
-	totallocal.puntos_local + totalvisita.puntos_visita as total_puntos_favor,
-	totallocal.puntos_local_contra + totalvisita.puntos_visita_contra as total_puntos_contra,
-	totallocal.ganados_local + totalvisita.ganados_visita as total_ganados,
-	totallocal.perdidos_local + totalvisita.perdidos_visita as total_perdidos,
-	totallocal.jugados_local + totalvisita.jugados_visita as total_jugados
-FROM
-	equipo,
-	torneoequipo,
-	(
-	select
-		l.local_id,
+	totallocal.puntos_local, 
+	totallocal.puntos_local_contra, 
+	totallocal.ganados_local, 
+	totallocal.perdidos_local, 
+	totallocal.jugados_local, 
+	
+	totalvisita.puntos_visita, 
+	totalvisita.puntos_visita_contra, 
+	totalvisita.ganados_visita, 
+	totalvisita.perdidos_visita, 
+	totalvisita.jugados_visita, 
+	
+	totallocal.puntos_local + totalvisita.puntos_visita as total_puntos_favor, 
+	totallocal.puntos_local_contra + totalvisita.puntos_visita_contra as total_puntos_contra, 
+	(totallocal.puntos_local + totalvisita.puntos_visita) - (totallocal.puntos_local_contra + totalvisita.puntos_visita_contra) as diferencia_puntos, 
+	totallocal.ganados_local + totalvisita.ganados_visita as total_ganados, 
+	totallocal.perdidos_local + totalvisita.perdidos_visita as total_perdidos, 
+	totallocal.jugados_local + totalvisita.jugados_visita as total_jugados 
+FROM 	
+	equipo, 
+	torneoequipo, 
+	( 
+	select 
+		l.local_id, 
 		j.torneo_id,
-		sum(l.puntos_local) as puntos_local,
-		sum(l.puntos_visitante) as puntos_local_contra,
-		count(*) as jugados_local,
-		SUM(CASE WHEN l.puntos_local>l.puntos_visitante THEN 1 ELSE 0 END) as ganados_local,
-		SUM(CASE WHEN l.puntos_visitante>l.puntos_local THEN 1 ELSE 0 END) as perdidos_local
-	from
-		partido l, jornada j
-	where
-		l.jornada_id = j.id
+		sum(l.puntos_local) as puntos_local, 
+		sum(l.puntos_visitante) as puntos_local_contra, 
+		count(*) as jugados_local, 
+		SUM(CASE WHEN l.puntos_local>l.puntos_visitante THEN 1 ELSE 0 END) as ganados_local, 
+		SUM(CASE WHEN l.puntos_visitante>l.puntos_local THEN 1 ELSE 0 END) as perdidos_local 
+	from 
+		partido l, jornada j 
+	where 
+		l.jornada_id = j.id 
 	group by l.local_id, j.torneo_id
-	) totallocal,
-	(
-	select
-		v.visitante_id,
+	) totallocal, 
+	( 
+	select 
+		v.visitante_id, 
 		j.torneo_id,
-		sum(v.puntos_visitante) as puntos_visita,
-		sum(v.puntos_local) as puntos_visita_contra,
-		count(*) as jugados_visita,
-		SUM(CASE WHEN v.puntos_visitante>v.puntos_local THEN 1 ELSE 0 END) as ganados_visita,
-		SUM(CASE WHEN v.puntos_local>v.puntos_visitante THEN 1 ELSE 0 END) as perdidos_visita
-	from
-		partido v, jornada j
-	where
-		v.jornada_id = j.id
+		sum(v.puntos_visitante) as puntos_visita, 
+		sum(v.puntos_local) as puntos_visita_contra, 
+		count(*) as jugados_visita, 
+		SUM(CASE WHEN v.puntos_visitante>v.puntos_local THEN 1 ELSE 0 END) as ganados_visita, 
+		SUM(CASE WHEN v.puntos_local>v.puntos_visitante THEN 1 ELSE 0 END) as perdidos_visita 
+	from 
+		partido v, jornada j 
+	where 
+		v.jornada_id = j.id 
 	group by v.visitante_id, j.torneo_id
-	) totalvisita
-WHERE
-	equipo.id = torneoequipo.equipo_id
-	and torneoequipo.equipo_id = totallocal.local_id
-	and torneoequipo.equipo_id = totalvisita.visitante_id;
+	) totalvisita 
+WHERE 
+	equipo.id = torneoequipo.equipo_id 
+	and torneoequipo.equipo_id = totallocal.local_id 
+	and torneoequipo.equipo_id = totalvisita.visitante_id
+ORDER BY
+	total_ganados, diferencia_puntos, total_puntos_favor;
---
+++ repositories/src/main/resources/com/codigoartesanal/entuliga/scripts/h2/user.sql
@@ -0,0 +1,47 @@
+CREATE TABLE IF NOT EXISTS USER (
+    USERNAME VARCHAR(25) PRIMARY KEY,
+    PASSWORD VARCHAR(70) NOT NULL,
+    ENABLED BIT NOT NULL
+);
+               
+INSERT INTO USER (USERNAME, PASSWORD, ENABLED) VALUES
+('jperez', '123456', 1),
+('jsoto', '$2a$10$GqqtbEuDi8YXzI1n8Zoqv.Upp61NP/Jy1fvPiMAgtcsyFuwc7N.AK', 1),
+('rolguin', '123456', 1),
+('sgarcia', '123456', 1),
+('jmolina', '123456', 1),
+('snaranjo', '123456', 1),
+('gduque', '123456', 1),
+('jsaenz', '123456', 1),
+('gloreto', '123456', 1),
+('omurillo', '123456', 1),
+('aosorno', '123456', 1),
+('cpalacio', '123456', 1),
+('hgonzalez', '123456', 1),
+('cmontoya', '123456', 1),
+('atabares', '123456', 1),
+('jlopez', '123456', 1);
+
+CREATE TABLE IF NOT EXISTS USER_ROLE (
+    ID BIGINT AUTO_INCREMENT PRIMARY KEY,
+    USERNAME VARCHAR(25) NOT NULL,
+    ROLE VARCHAR(25) NOT NULL
+);
+
+INSERT INTO USER_ROLE (ID, USERNAME, ROLE) VALUES
+(1, 'jperez', 'ADMIN'),
+(2, 'jsoto', 'GERENTE'),
+(3, 'rolguin', 'JUGADOR'),
+(4, 'sgarcia', 'JUGADOR'),
+(5, 'jmolina', 'JUGADOR'),
+(6, 'snaranjo', 'JUGADOR'),
+(7, 'gduque', 'JUGADOR'),
+(8, 'jsaenz', 'JUGADOR'),
+(9, 'gloreto', 'JUGADOR'),
+(10, 'omurillo', 'JUGADOR'),
+(11, 'aosorno', 'JUGADOR'),
+(12, 'cpalacio', 'JUGADOR'),
+(13, 'hgonzalez', 'JUGADOR'),
+(14, 'cmontoya', 'JUGADOR'),
+(15, 'atabares', 'JUGADOR'),
+(16, 'jlopez', 'JUGADOR');
