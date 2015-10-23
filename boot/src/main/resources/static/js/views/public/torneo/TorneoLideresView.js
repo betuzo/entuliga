3dcc7b326857717c2475509dfd39d6e889697c9e
betuzo <betotsol@gmail.com>
2015-10-22 22:26:08 -0500

    servicio de top five listo

--- boot/src/main/resources/static/js/views/public/torneo/TorneoLideresView.js
+++ boot/src/main/resources/static/js/views/public/torneo/TorneoLideresView.js
@@ -2,8 +2,10 @@ define([
 	'jquery',
 	'bootstrap',
 	'core/BaseView',
+    'collections/EstadisticasCollection',
+    'views/public/torneo/RowEstadisticaJugadorView',
 	'text!templates/public/torneo/tplTorneoLideres.html'
-], function($, bootstrap, BaseView, tplTorneoLideres){
+], function($, bootstrap, BaseView, EstadisticasCollection, RowEstadisticaJugadorView, tplTorneoLideres){
 
 	var TorneoLideresView = BaseView.extend({
 	    el: '#torneo-lideres',
@@ -14,11 +16,43 @@ define([
 
         initialize: function(modelo) {
             this.model = modelo;
+
+            this.lideres = new EstadisticasCollection();
+            this.lideres.setTorneo(this.model);
+            this.listenTo(this.lideres, 'add', this.agregarLider);
+            this.listenTo(this.lideres, 'sync', this.syncLideres);
+
+            this.lideres.fetch();
+
             this.render();
         },
 
         render: function() {
             this.$el.html(this.template(this.model.toJSON()));
+        },
+
+        agregarLider: function(modelo) {
+            var vista = new RowEstadisticaJugadorView(modelo);
+            $("#torneo-tabla-posiciones").find('tbody:last').append(vista.render().$el);
+        },
+
+        syncLideres: function() {
+
+        },
+
+        getTableByTipoEstadistica: function(tipoEstadistica) {
+            switch (tipoEstadistica) {
+                case 1:
+                    return "torneo-lideres-puntos";
+                case 2:
+                    return "torneo-lideres-rebotes";
+                case 3:
+                    return "torneo-lideres-asistencias";
+                case 4:
+                    return "torneo-lideres-bloqueos";
+                case 5:
+                    return "torneo-lideres-robos";
+            }
         }
 	});
 
