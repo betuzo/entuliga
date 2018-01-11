0bcf1cdee5f1ec8f5a372fbff8bff0c19681cf69
adnlos <small_los@hotmail.com>
2018-01-11 14:25:07 -0600

    main admin view

--- web/src/main/resources/static/js/views/private/MainAdminNavView.js
+++ web/src/main/resources/static/js/views/private/MainAdminNavView.js
@@ -6,33 +6,27 @@ define([
 ], function($, BaseView, tplMainAdminNav, Session) {
 
   var MainAdminNavView = BaseView.extend({
-    // el: $("#hotel-nav"),
 
     template: _.template(tplMainAdminNav),
 
     events: {
       'click #logout': 'logout',
       'click #home': 'homeAdmin',
-      'click li.nav-item-entuliga': 'activeMenu'
+      'click .nav-item-entuliga': 'activeMenu'
     },
 
     initialize: function() {
-      // this.listenTo("reset", this.updateView);
-      // this.listenTo('destroy', this.remove);
-      // this.bind("reset", this.remove());
       this.render();
     },
 
     render: function() {
       this.$el.html(this.template());
-      // this.remove();
       return this;
     },
 
     logout: function() {
       Session.logout(function(response) {
         Backbone.history.navigate('', { trigger: true });
-        window.location.reload();
       });
     },
 
@@ -41,20 +35,9 @@ define([
     },
 
     activeMenu: function(event) {
+      $('.nav-item-entuliga').removeClass('active');
       $(event.target).parent().addClass('active');
-      // $('.nav-item-entuliga').removeClass('active');
     },
-
-    destroy_view: function() {
-      // COMPLETELY UNBIND THE VIEW
-      this.undelegateEvents();
-
-      this.$el.removeData().unbind();
-
-      // Remove view from DOM
-      this.remove();
-      Backbone.View.prototype.remove.call(this);
-    }
   });
 
   return MainAdminNavView;
