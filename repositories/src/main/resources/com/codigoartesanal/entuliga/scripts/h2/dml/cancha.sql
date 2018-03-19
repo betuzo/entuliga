6e1fe883cd2c10f286befb1a28821de397939c23
adnlos <small_los@hotmail.com>
2018-03-18 23:21:01 -0600

    data dll h2

--- repositories/src/main/resources/com/codigoartesanal/entuliga/scripts/h2/dml/cancha.sql
+++ repositories/src/main/resources/com/codigoartesanal/entuliga/scripts/h2/dml/cancha.sql
@@ -1,4 +1,5 @@
-alter sequence cancha_id_seq restart with 3;
+alter sequence cancha_id_seq restart with 4;
 INSERT INTO CANCHA (ID, ADMIN_ID, NOMBRE, ALIAS, DESCRIPCION, GEOLOCATION_ID) VALUES
 (1, 'jperez@tu.me', 'El Calvario Uno', 'Calvario 1', 'Techada, con luz, sin grada', 1),
-(2, 'jperez@tu.me', 'El Calvario Dos', 'Calvario 2', 'Sin techo, sin luz, sin grada', 2);
+(2, 'jperez@tu.me', 'El Calvario Dos', 'Calvario 2', 'Sin techo, sin luz, sin grada', 2),
+(3, 'jperez@tu.me', 'El Calvario Dos', 'Calvario 2', 'Sin techo, sin luz, sin grada', 3);
--- repositories/src/main/resources/com/codigoartesanal/entuliga/scripts/h2/dml/geolocation.sql
+++ repositories/src/main/resources/com/codigoartesanal/entuliga/scripts/h2/dml/geolocation.sql
@@ -1,4 +1,5 @@
-alter sequence geolocation_id_seq restart with 3;
+alter sequence geolocation_id_seq restart with 4;
 INSERT INTO GEOLOCATION (ID, CALLE, NO_EXTERIOR, NO_INTERIOR, COLONIA_ID, LONGITUDE, LATITUDE) VALUES
-(1, 'Claro Tapia', '15', null, 1, 2234234, 2342343),
-(2, 'Felipe Angeles', '478', null, 1, 2234234, 2342343);
+(1, 'Claro Tapia', '15', null, 1, 124.3516731, 11.3331243),
+(2, 'Felipe Angeles', '478', null, 1, 124.3516731, 11.3331243),
+(3, 'Maple Wood', '1646', '04324', 3, 124.3516731, 11.3331243);
