af8da32b9ad1a7e65a5a525444ea97d24808b55f
Roberto Olguin <roberto.olguin@tcpip.tech>
2018-04-06 13:02:43 -0400

    quitando eureka... por ahora

--- web/src/main/java/com/codigoartesanal/entuliga/Application.java
+++ web/src/main/java/com/codigoartesanal/entuliga/Application.java
@@ -9,7 +9,6 @@ import com.codigoartesanal.entuliga.config.security.SecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -20,7 +19,6 @@ import org.springframework.context.annotation.Import;
         SecurityConfig.class, InfrastructureConfig.class, MailConfig.class})
 @EnableConfigurationProperties
 @SpringBootApplication
-@EnableDiscoveryClient
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
